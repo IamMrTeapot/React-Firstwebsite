3daf1336f47e3523fb209972ff2640f47f7545b1
Thamon Nantasen <mrteapot2016@gmail.com>
2022-07-23 08:27:58 +0700

    Add Font and do some css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      If this message show on browser, Your repo and react app work
-      functionally.
+      <h1 id="TopText">The Random</h1>
+      <h1 id="BotText">Photo Booth</h1>
     </div>
   );
 }
